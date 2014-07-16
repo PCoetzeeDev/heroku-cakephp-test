cc27b7245b621aaaf13e0ab089e48032e4a8fd3e
Petri Coetzee <petri@semantica.co.za>
2014-07-16 16:38:17 +0200

    cipher and salt changed

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '863ASDF4df2yJfIxfs2guVoUubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768593546543513285411851329683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
