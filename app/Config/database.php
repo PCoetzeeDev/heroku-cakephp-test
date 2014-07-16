468b6bcef560e4a298da55ecb2a6eb5732670b6f
Petri Coetzee <petri@semantica.co.za>
2014-07-16 16:12:32 +0200

    configured database connection...hopefully

--- app/Config/database.php
+++ app/Config/database.php
@@ -63,26 +63,31 @@
  * A key/value array of driver specific connection options.
  */
 class DATABASE_CONFIG {
-
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
-
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
+	function __construct() {
+ 
+        $url = parse_url(getenv('DATABASE_URL'));
+ 
+        $this->default = array(
+            'datasource' => 'Database/Postgres,
+            'persistent' => false,
+            'host' => $url['host'],
+            'login' => $url['user'],
+            'password' => $url['pass'],
+            'database' => substr($url['path'],1),
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+ 
+        $this->test = array(
+            'datasource' => 'Database/Postgres',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'user',
+            'password' => 'password',
+            'database' => 'test_database_name',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+ 
+    }
 }
